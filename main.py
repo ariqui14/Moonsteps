0302dd73b81f4de6c0353f61902b6ca13f1fa91d
Ariel Quinain <quinainar@gmail.com>
2024-01-29 11:58:57 -0800

    Removed New Task code from original tutorial

--- main.py
+++ main.py
@@ -57,24 +57,6 @@ def todo_list():
     
 #{{TaskNo}}, {{TaskName}}, and {{Task}}
 
-@route('/new', method='GET')
-def new_item():
-    
-    if request.GET.save:
-        new = request.GET.task.strip()
-        conn = sqlite3.connect('todo.db')
-        c = conn.cursor()
-
-        c.execute("INSERT INTO todo (task, status) VALUES (?,?)", (new, 1))
-        new_id = c.lastrowid
-
-        conn.commit()
-        c.close()
-
-        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
-    else:
-        return template('new_task.tpl')
-    
 
 @route('/edit/<no:int>', method='GET')
 def edit_item(no):
